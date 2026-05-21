69cac00d16f74c5d95bb92e6bfbbf56ae1fd0ae0
Nepthys14 <281753059+Nepthys14@users.noreply.github.com>
2026-05-21 15:42:18 -0400

    Supprime exercices.py

--- exercices.py
+++
@@ -1,4 +0,0 @@
-with open("journal_test.txt", "a") as fichier:
-    fichier.write("Aujourd'hui j'ai appris les fichiers.\n")
-    fichier.write("Je comprends mieux comment sauvegarder des données.\n")
-    fichier.write("------------------------------\n")
